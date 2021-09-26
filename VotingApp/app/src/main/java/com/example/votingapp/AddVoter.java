5e7e5461478d57202d03e979ca74697524739955
ghstlovrS <sandunikasenavirathna@gmail.com>
2021-09-26 23:40:03 +0530

    initial

---
+++ VotingApp/app/src/main/java/com/example/votingapp/AddVoter.java
@@ -0,0 +1,15 @@
+package com.example.votingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddVoter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_voter);
+        getSupportActionBar().hide();
+    }
+}
