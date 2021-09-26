b4a8e0bdfbb2fac78c87f436d811d2fb6233fc7e
user <it20224202@my.sliit.lk>
2021-09-26 19:55:23 +0530

    initial

---
+++ VotingApp/app/src/main/java/com/example/votingapp/AddCandidate.java
@@ -0,0 +1,15 @@
+package com.example.votingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCandidate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_candidate);
+        getSupportActionBar().hide();
+    }
+}
